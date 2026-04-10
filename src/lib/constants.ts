eb0134883fcaeaf3978e50aa233716ee4f58edd1
Emmanuel Sylvester <38680059+emansyl@users.noreply.github.com>
2026-04-10 10:06:54 -0400

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,10 @@
 export const HBS_EMAIL_DOMAINS = [
   "@mba2026.hbs.edu",
   "@mba2027.hbs.edu",
+  "@mba2028.hbs.edu",
+  "@mba2029.hbs.edu",
+  "@mba2030.hbs.edu",
+  "@mba2031.hbs.edu"
 ] as const;
 
 export function isValidHBSEmail(email: string): boolean {
